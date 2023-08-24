6554ea0e22d80d6b4f37724408c8444477f9e046
abigael mwelu <abigael.mutuku@student.moringaschool.com>
2023-08-24 11:59:03 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -13,3 +13,41 @@ APPROVED_BREEDS = [
 
 class Dog:
     pass
+class Dog:
+    def __init__(self):
+        self._name = None
+        self._breed = None
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        approved_breeds = ["French Bulldog","Chihuahua","Pointer" ]  # approved breeds
+        if value in approved_breeds:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+
+# Create an instance of the Dog class
+dog = Dog()
+
+# Testing the properties
+dog.name = "Buddy"  # Valid name
+dog.breed = "Pug"  # Valid breed
+
+dog.name = "This is a very long name that exceeds the limit"  # Invalid name
+dog.breed = "Golden Retriever"  # Invalid breed
+
--- lib/person.py
+++ lib/person.py
@@ -17,3 +17,43 @@ APPROVED_JOBS = [
 
 class Person:
     pass
+class Person:
+    approved_jobs = ["Marketing", "Sales", "Finance"]  # approved jobs
+
+    def __init__(self):
+        self._name = None
+        self._job = None
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) < 25:
+            self._name = value.title()
+        else:
+            print("Name must be a string under 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in self.approved_jobs:
+            self._job = value
+        else:
+            print("The job must be in the following list of jobs")
+
+# Create an instance of the Person class
+person = Person()
+
+# Testing the properties
+person.name = "john"  # Valid name
+person.job = "Customer Service"  # Valid job
+
+person.name = "this is a very long name that exceeds the limit"  # Invalid name
+person.job = "Writer"  # Invalid job
+
+
